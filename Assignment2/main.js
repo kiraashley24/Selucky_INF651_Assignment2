402dbc2024240f428397e22d3037ae97366c0fff
kiraashley24 <kiraselucky@gmail.com>
2023-06-25 18:01:59 -0600

    initial commit

---
+++ Assignment2/main.js
@@ -0,0 +1,80 @@
+//Question 1
+//The variable myNumber should by defined.
+
+let myNumber;
+
+//Question 2
+//The variable myNumber should be type number.
+//The variable myNumber should be equal to 5.
+
+myNumber = 5;
+
+
+//Question 3
+//The variable myOtherNumber should by defined.
+//The variable myOtherNumber should be type number.
+//The variable myOtherNumber should be equal to 5.
+
+let myOtherNumber;
+myOtherNumber = 5;
+
+//Question 4
+//The variable myTotal should by defined.
+//The variable myTotal should be type number.
+//The variable myTotal should be equal to the product of myNumber and myOtherNumber.‣
+
+let myTotal;
+myTotal = myNumber * myOtherNumber;
+
+//Question 5
+//The variable myName should by defined.
+//The variable myName should be type string.
+//The variable myCombo should by defined.
+//The variable myCombo should be type string.
+//The variable myCombo should be equal to the sum of myNumber and myOtherNumber.‣
+
+let myName; 
+let myCombo;
+myCombo = "myNumber + myOtherNumber";
+//Would be myCombo = myNumber + myOtherNumber if adding actual numbers/integers
+
+
+//Question 6
+//The variable myOtherCombo should by defined.
+//The variable myOtherCombo should be type number.
+//The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.‣
+
+let myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+
+//Question 7
+//The variable myRemainder should by defined.
+//The variable myRemainder should be type number.
+//The variable myRemainder should be equal to the remainder of myNumber divided by 5.
+
+let myRemainder;
+myRemainder = myNumber % 5;
+
+//Question 8
+//The variable i should by defined.
+//The variable i should be type number.
+//The variable i should be equal to 2.
+
+let i;
+i = 2;
+
+//Question 9
+//The variable int should by defined.
+//The variable int should be type number.
+//The variable int should be equal to 4.
+
+let int;
+int = 4;
+
+//Question 10
+//The variable myBoolean should by defined.
+//The variable myBoolean should be type boolean.
+//The variable myBoolean should be false.
+
+let myBoolean;
+myBoolean = false;
